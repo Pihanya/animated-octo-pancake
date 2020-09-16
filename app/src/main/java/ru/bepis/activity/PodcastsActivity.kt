a2a1163e716bde7e3a571efd3b30212497c9cb2f
Igor Anokhin <igr.anokhin@gmail.com>
2020-09-17 02:53:49 +0300

    commit

--- app/src/main/java/ru/bepis/activity/PodcastsActivity.kt
+++ app/src/main/java/ru/bepis/activity/PodcastsActivity.kt
@@ -16,14 +16,7 @@ class PodcastsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
-        setSupportActionBar(findViewById(R.id.toolbar))
         requestStoragePermission()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
     }
 
     fun onPodcastCreateButtonClicked(view: View) {
