5c80dc98a186fb9f7e98f44f5642156167aa1933
pihanya <gostevvm@yandex.ru>
2020-09-17 02:18:57 +0300

    Add possibility to record

--- app/src/main/java/ru/bepis/activity/PodcastsActivity.kt
+++ app/src/main/java/ru/bepis/activity/PodcastsActivity.kt
@@ -1,18 +1,23 @@
 package ru.bepis.activity
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import ru.bepis.R
 
 class PodcastsActivity : AppCompatActivity() {
+
+    private val REQUEST_ID_PERMISSIONS = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcasts)
         setSupportActionBar(findViewById(R.id.toolbar))
+        requestStoragePermission()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -22,7 +27,17 @@ class PodcastsActivity : AppCompatActivity() {
     }
 
     fun onPodcastCreateButtonClicked(view: View) {
-        val intent = Intent(this, NewPodcastActivity::class.java)
+        val intent = Intent(this, AudioTrimmerActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun requestStoragePermission() {
+        ActivityCompat.requestPermissions(
+            this@PodcastsActivity, arrayOf(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.RECORD_AUDIO
+            ),
+            REQUEST_ID_PERMISSIONS
+        )
     }
 }
