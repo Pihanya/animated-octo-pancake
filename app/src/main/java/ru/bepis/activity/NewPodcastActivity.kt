74894a696d9412fa88711d338a8b3ca9c2da9383
pihanya <gostevvm@yandex.ru>
2020-09-16 22:15:13 +0300

    Initial commit

---
+++ app/src/main/java/ru/bepis/activity/NewPodcastActivity.kt
@@ -0,0 +1,12 @@
+package ru.bepis.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.bepis.R
+
+class NewPodcastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_podcast)
+    }
+}
---
+++ app/src/main/java/ru/bepis/activity/PodcastsActivity.kt
@@ -0,0 +1,28 @@
+package ru.bepis.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import ru.bepis.R
+
+class PodcastsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_podcasts)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    fun onPodcastCreateButtonClicked(view: View) {
+        val intent = Intent(this, NewPodcastActivity::class.java)
+        startActivity(intent)
+    }
+}
