03c8f246681f9dfeba343b2c368ad5752fbc8582
Igor Anokhin <igr.anokhin@gmail.com>
2020-09-17 04:44:34 +0300

    finish NewPodcast

--- app/src/main/java/ru/bepis/activity/NewPodcastActivity.kt
+++ app/src/main/java/ru/bepis/activity/NewPodcastActivity.kt
@@ -2,14 +2,18 @@ package ru.bepis.activity
 
 import android.app.Activity
 import android.content.Intent
+import android.database.Cursor
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_new_podcast.*
 import ru.bepis.R
 import java.io.BufferedInputStream
 import java.io.InputStream
+
 
 class NewPodcastActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,18 +42,38 @@ class NewPodcastActivity : AppCompatActivity() {
         if (requestCode === 1 && resultCode == Activity.RESULT_OK) {
             if (data!!.data != null) {
                 val uri = data!!.data
-                val inputStream: InputStream = contentResolver.openInputStream(uri!!)!!
-                val bufferedInputStream = BufferedInputStream(inputStream);
-                val bmp = BitmapFactory.decodeStream(bufferedInputStream);
 
 //                fundView.image = bmp
 
-                imageView.setImageBitmap(bmp)
-                loadedImageLayout.visibility = View.VISIBLE
-                iconViewForm.visibility = View.GONE
+                val name = getFileName(uri!!)
+                trackName.text = name
+                pickedTrackLayout.visibility = View.VISIBLE
+                TrackForm.visibility = View.GONE
             }
             //Now you can do whatever you want with your inpustream, save it as file, upload to a server, decode a bitmap...
         }
+    }
+
+    fun getFileName(uri: Uri): String? {
+        var result: String? = null
+        if (uri.getScheme().equals("content")) {
+            val cursor: Cursor? = contentResolver.query(uri, null, null, null, null)
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                }
+            } finally {
+                cursor!!.close()
+            }
+        }
+        if (result == null) {
+            result = uri.getPath()
+            val cut = result!!.lastIndexOf('/')
+            if (cut != -1) {
+                result = result.substring(cut + 1)
+            }
+        }
+        return result
     }
 
     fun onPickImageClick(view: View) {
